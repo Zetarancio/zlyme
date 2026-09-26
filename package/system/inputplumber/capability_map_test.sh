4c6de110ca51cd55093ca1dfc86ec1d4ba51e6b4
Zetarancio <alessandro.zucca01@gmail.com>
2026-09-26 09:12:04 +0200

    input: map Flip L2 R2 to xb360 axes

--- package/system/inputplumber/capability_map_test.sh
+++ package/system/inputplumber/capability_map_test.sh
@@ -3,11 +3,24 @@ set -eu
 dir=$(CDPATH= cd -- "$(dirname "$0")" && pwd)
 map=$dir/zlyme_miyoo_flip.yaml
 dev=$dir/20-zlyme_miyoo_flip.yaml
-for pair in "BTN_DPAD_UP DPadUp" "BTN_DPAD_DOWN DPadDown" "BTN_DPAD_LEFT DPadLeft" "BTN_DPAD_RIGHT DPadRight"; do
-	set -- $pair
-	grep -q "event_code: $1" "$map"
-	grep -q "button: $2" "$map"
-done
+got=$(mktemp)
+want=$(mktemp)
+trap 'rm -f "$got" "$want"' EXIT
+awk '
+	/event_code:/ { code = $2; vt = "" }
+	/value_type:/ { vt = $2 }
+	/button:/ { print code, vt, "button", $2 }
+	/name: LeftTrigger/ { print code, vt, "trigger", "LeftTrigger" }
+	/name: RightTrigger/ { print code, vt, "trigger", "RightTrigger" }
+' "$map" >"$got"
+printf '%s\n' \
+	"BTN_DPAD_UP button button DPadUp" \
+	"BTN_DPAD_DOWN button button DPadDown" \
+	"BTN_DPAD_LEFT button button DPadLeft" \
+	"BTN_DPAD_RIGHT button button DPadRight" \
+	"BTN_TL2 trigger trigger LeftTrigger" \
+	"BTN_TR2 trigger trigger RightTrigger" >"$want"
+cmp -s "$want" "$got"
 grep -q 'id: zlyme_miyoo_flip' "$map"
 grep -q 'capability_map_id: zlyme_miyoo_flip' "$dev"
 grep -q 'auto_manage: false' "$dev"
