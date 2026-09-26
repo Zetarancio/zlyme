c55ee25e142134360b3eb7980e6bb3fb394a0524
Zetarancio <alessandro.zucca01@gmail.com>
2026-09-26 12:56:06 +0200

    input: correct Flip X and Y virtual buttons

--- package/system/inputplumber/capability_map_test.sh
+++ package/system/inputplumber/capability_map_test.sh
@@ -22,8 +22,8 @@ printf '%s\n' \
 	"BTN_TR2 trigger trigger RightTrigger" \
 	"BTN_EAST button button South" \
 	"BTN_SOUTH button button East" \
-	"BTN_NORTH button button West" \
-	"BTN_WEST button button North" >"$want"
+	"BTN_NORTH button button North" \
+	"BTN_WEST button button West" >"$want"
 cmp -s "$want" "$got"
 grep -q 'ea60d873cca17edd1cb655ede26f557108135252/rootfs/usr/share/inputplumber/schema/capability_map_v2.json' "$map"
 if grep -q 'InputPlumber/main/' "$map"; then
